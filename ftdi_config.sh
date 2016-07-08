ab1ab115a1d56f5eee9906ba2ca9c73581b18fde
odroid <odroid@odroid.com>
2016-07-08 11:30:10 +0000

    update to ftdi_conf.sh

--- ftdi_config.sh
+++ ftdi_config.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-#begin
-echo "Configuring FTDI Drivers."
-sudo rmmod ftdi_sio
+usrname="odroid"
 
-return 0
+if [ "$(id -u)" = "0" ]
+then
+    echo
+    echo "This script will add the ftdi_sio drivers to the blacklist so that the D2XX";
+    echo "will work with the lens driver and GPS devices.";
+    echo 
+    echo
+else
+    echo
+    echo "This script needs to be run as root.";
+    echo "eg.";
+    echo "sudo ftdi_config.sh";
+    echo
+    exit 0
+fi
 
+BlacklistFile="/etc/modprobe.d/blacklist_ftdi.conf";
+echo
+echo "Writing the blacklist file.";
+
+# Add gpio control to odroid user to allow usage of the gpio pins
+echo "blacklist ftdi_sio" 1>$BlacklistFile
+
+
+
+echo
+echo "Configuration complete. A reboot may be required on some systems for changes to take effect";
+echo
+
+exit 0
